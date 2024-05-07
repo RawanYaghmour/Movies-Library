d01dfbb635acd4df171a760ac764cff5f0c27fe3
RawanYaghmour <Rawan.yag02@gmail.com>
2024-05-07 23:11:17 +0000

    add lab 13

--- index.js
+++ index.js
@@ -1,192 +1,70 @@
-//1-require express framework
+'use strict';
+//1-require framework
 const express =require('express');
-
-//2-invoke express 
-const app = express()
-const cors = require('cors')
-const axios = require('axios')
-require('dotenv').config()
-const port = process.env.PORT
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+const port = process.env.PORT ||3001
 const apiKey= process.env.API_KEY
-app.use(cors())
-const  recipesData = require('./Movie Data/data.json')
-
-//3-run server make it listening & ready for any request
-app.listen(port, () =>{
-    console.log("welcome to movie website "+port);
-})
+app.use(cors());
+//const  recipesData = require('./Movie Data/data.json')
 
 
+//************************************************************* 
+//!-------------------------Lap 13------------------------------!
+
+const { Client } = require('pg')
+const url ='postgres://rawan:0000@localhost:5432/moviedatabase'
+const client = new Client(url)
+
+//Route
+app.get('/', homeHandler);
+app.post('/addMovie', addMovieHandler);
+app.get('/getMovies', getMoviesHandler);
+
+//handelers
+function homeHandler(req, res){
+    res.send("welcome to home");
+}
+function addMovieHandler(req, res){
+    console.log(req.body)
+    //to collect the data:
+    //const title= req.body.title;
+    //const time= req.body.time;
+    //const image= req.body.image;
+
+    const {title, time, image }= req.body //destructuring ES6
+    const sql=`INSERT INTO Movies (title, time, image)
+    VALUES ($1, $2, $3) RETURNING *;`
+    const values= [title, time, image]
+    client.query(sql, values).then((result)=>{
+        console.log(result.rows);
+        res.status(201).json(result.rows)
+    }).catch()
 
 
-//!-------------------------Lap 11------------------------------!
+   // res.send("data recived");
+}
+function getMoviesHandler(req, res){
+    const sql= `SELECT * FROM Movies;`
+
+    client.query(sql).then((result)=>{
+        const data = result.rows
+        res.json(data)
+    })
+    .catch()
+    }
 
 
-
-//routing
-//Home Page Endpoint:
-
-app.get('/', firstRouteHandler);
-function firstRouteHandler(req,res){
-// res.send("welcome to first rout.");
-let reCheapeRecipe = new Recipe(recipesData.title, recipesData.poster_path, recipesData.overview);
-  res.send(reCheapeRecipe)
+//listener
+client.connect().then(()=>{
+    app.listen(port, () =>{
+        console.log("listening to port "+port);
+    })
 }
 
-//constructor
-function Recipe(title, poster_path,overview){
-    this.title=title
-    this.poster_path=poster_path
-    this.overview=overview
-}
-
-//Favorite Page Endpoint: “/favorite”
-
-app.get('/favorite', secondRouteHandler);
-function secondRouteHandler(req,res){
-    res.send("Welcome to Favorite Page ");
-}
-
-
-//Handle errors
-
-// Function to handle "ServerError" error (status 500)
-function handleServerError( req, res) {
-    res.status(500).json({
-        status: 500,
-        responseText: 'Sorry, something went wrong'
-    });
-}
-
-// Function to handle "page not found" error (status 404)
-function handleNotFoundError(req, res) {
-    res.status(404).json({
-        status: 404,
-        responseText: 'Page not found'
-    });
-}
-
-
-
-//*************************************************************************** 
-
-//!-------------------------Lap 12------------------------------!
-
-//Constructor
-function Movie(id, title, release_data, poster_path, overview) {
-    this.id = id;
-    this.title = title;
-    this.release_data = release_data;
-    this.poster_path = poster_path;
-    this.overview = overview;
-}
-
-// trending endpoint
-app.get("/trending", handelTrending);
-function handelTrending(req, res) {
-    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&language=en-US`;
-    axios.get(url)
-        .then(result => {
-            console.log(result.data.results);
-            let trending = result.data.results.map(trend => {
-                return new Movie(
-                    trend.id,
-                    trend.title,
-                    trend.release_data,
-                    trend.poster_path,
-                    trend.overview
-                );
-            });
-            res.json(trending);
-        })
-        .catch(error => {
-            console.error( error);
-            res.status(500).json('Internal Server Error');
-        });
-}
-
-
-
-//search
-app.get("/search", searchHandler);
-function searchHandler(req, res) {
-const url= `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=The&page=2`;
-axios.get(url)
-        .then(result => {
-            console.log(result.data.results);
-            let searching = result.data.results.map(trend => {
-                return new Movie(
-                    trend.id,
-                    trend.title,
-                    trend.release_data,
-                    trend.poster_path,
-                    trend.overview
-                );
-            });
-            res.json(searching);
-        })
-        .catch(error => {
-            console.error( error);
-            res.status(500).json('Internal Server Error');
-        });
-}
-
-
-
-
-//Movie upcoming
-app.get("/upcoming", upcomingHandler);
-
-function upcomingHandler(req,res){
-
-const url=`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`;
-
-axios.get(url)
-        .then(result => {
-            console.log(result.data.results);
-            let upcoming = result.data.results.map(trend => {
-                return new Movie(
-                    trend.id,
-                    trend.title,
-                    trend.release_data,
-                    trend.poster_path,
-                    trend.overview
-                );
-            });
-            res.json(upcoming);
-        })
-        .catch(error => {
-            console.error( error);
-            res.status(500).json('Internal Server Error');
-        });
-}
-
-
-//4rd rout
-app.get("/movie", movieHandler);
-function movieHandler(req, res) {
-const url= `https://api.themoviedb.org/3/movie/157336?api_key=${apiKey}&append_to_response=videos`;
-axios.get(url)
-        .then(result => {
-            console.log(result.data.results);
-            let movie = result.data.results.map(trend => {
-                return Movie(
-                    trend.id,
-                    trend.title,
-                    trend.release_data,
-                    trend.poster_path,
-                    trend.overview
-                );
-            });
-            res.json(movie);
-        })
-        .catch(error => {
-            console.error( error);
-            res.status(500).json('Internal Server Error');
-        });
-}
-
-
-
-app.use(handleServerError);
-app.use(handleNotFoundError);
+).catch()
---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Movies (
+    title VARCHAR(255),
+    time VARCHAR(255),
+    image VARCHAR(255),
+    personal_comment: "This movie is a masterpiece! The storyline and character development are phenomenal."
+   
+);
