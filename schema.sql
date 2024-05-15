06e72d7cfb38df0723a5cae2daf1bb8828842f86
RawanYaghmour <Rawan.yag02@gmail.com>
2024-05-15 02:21:15 +0000

    add change

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE Movie (
+CREATE TABLE tMovie (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    time VARCHAR(255),
-    image VARCHAR(255),
+    release_date VARCHAR(255),
+    poster_path VARCHAR(255),
+    overview VARCHAR(255),
     personal_comment TEXT 
 );
